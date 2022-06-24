14fb338618692f3820ba755b40f2e8ccc6b60e44
Posoroko <eric@posoroko.com>
2022-06-24 16:08:58 +0200

    cleanup

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -8,18 +8,25 @@ function editNav() {
 }
 
 // DOM Elements
+
         const modalbg = document.querySelector(".bground");
         const modalBtn = document.querySelectorAll(".modal-btn");
         const formData = document.querySelectorAll(".formData");
 
 //modal element
+
         const close_span = document.querySelector('.close')
         const btnSubmit_input = document.querySelector('.btn-submit')
         const reserve_form = document.getElementById('reserve')
         const checkbox1_input = document.getElementById('checkbox1')
         const submitBtn_input = document.getElementById('submitBtn')
+
+//error message 
         const conditionsError_p = document.getElementById('conditionsError')
         const locationError_p = document.getElementById('locationError')
+
+//confirmation panel elements
+
         const formPanel_div = document.getElementById('formPanel')
         const confirmationBox_div = document.querySelector('.confirmationBox')
         const confirmationCloseButton_btn = document.querySelector('.confirmationCloseButton')
@@ -27,23 +34,21 @@ function editNav() {
 // launch modal event
         modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
-// launch modal form
+// open - close  modal
         function launchModal() {
           modalbg.style.display = "block";
         }
-        const launchConfirmationModal = () => {
-          modalbg.style.display = "block";
-        }
 
-// close Modal
         close_span.addEventListener('click', () => {
           modalbg.style.display = "none";
           reserve_form.reset()
         })
+
+// confirmation panel
+        
         confirmationCloseButton_btn.addEventListener('click', () => {
           modalbg.style.display = "none";
           reserve.style.display = "block";
-          
           confirmationBox_div.style.display = "none";
         })
 
@@ -51,23 +56,26 @@ function editNav() {
 //submit form
 
 submitBtn.addEventListener('click', () => {
-  if (!checkbox1_input.checked) {
-    conditionsError_p.style.display = "block";
-  } else {
-    conditionsError_p.style.display = "none";
-  }
-  if(!reserve_form.location.value) {
-    locationError_p.style.display = "block";
-  } else {
-    locationError_p.style.display = "none";
-  }
+    if (!checkbox1_input.checked) {
+        conditionsError_p.style.display = "block";
+    } else {
+        conditionsError_p.style.display = "none";
+    }
+    if(!reserve_form.location.value) {
+        locationError_p.style.display = "block";
+    } else {
+        locationError_p.style.display = "none";
+    }
 })
 
 const validate = (e) => {  
   e.preventDefault()
 
+  //simulate form submission
   console.log("Form submitted to server. Thank you!")
   reserve_form.reset()
+
+  //switch to confirmation panel
   reserve.style.display = "none";
   confirmationBox_div.style.display = "grid";
 }
